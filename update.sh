c901e5b9e45f561759aa3881b3d192b9fac7cc38
momiji <momiji@github.com>
2023-08-18 09:14:45 +0200

    fix version increment algo

--- update.sh
+++ update.sh
@@ -78,10 +78,10 @@ update() {
         go mod tidy
         go mod verify
         go test ./...
-        pre=${last%.*}
+        pre=$( echo "$last" | sed "s/[0-9]*$//" )
         len=${#pre}
-        ver=${last:$((len+1))}
-        new=$pre.$((ver+1))
+        ver=${last:$len}
+        new=$pre$((ver+1))
         info "$prj: create new tag $new"
         run git tag "$new"
         run git push --tags
